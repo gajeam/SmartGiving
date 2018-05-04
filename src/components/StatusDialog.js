ab4f43e3243c3d6c0f84fd2e51771f0ce71a4ac2
Gabe Nicholas <gabe.nicholas@gmail.com>
2018-05-03 20:14:26 -0700

    Added basic status dialog

---
+++ src/components/StatusDialog.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Button from 'material-ui/Button'
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog'
+
+class StatusDialog extends Component {
+	render() {
+		return (
+			<Dialog
+				open={this.props.open}
+				onClose={this.props.onClose}
+		        >
+                <DialogTitle id="alert-dialog-title">{this.props.error === undefined ? this.props.title : "Error"}</DialogTitle>
+		        <DialogContent>
+		        	<DialogContentText id="alert-dialog-description">
+		        		{this.props.error === undefined ? this.props.content : this.props.error}
+		            </DialogContentText>
+		        </DialogContent>
+				<DialogActions>
+		            <Button onClick={this.props.onClose} color="primary" autoFocus>
+		              Got it
+		            </Button>
+				</DialogActions>
+			</Dialog>       
+		)
+	}
+}
+
+StatusDialog.propTypes = {
+  open: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  error: PropTypes.any
+}
+
+export default StatusDialog
