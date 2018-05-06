656d36be3f375a669b4a105e2346406d7736ff12
root <root@DESKTOP-FC75PKR.localdomain>
2018-05-06 10:04:36 -0700

    edited video

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -42,7 +42,7 @@ class Home extends Component {
 			    </div>
 
 				<div className="home-intro-video">
-					<ReactPlayer url="https://youtu.be/g83mXlikm-8"/>
+					<ReactPlayer url="https://www.youtube.com/watch?v=-FWwdGPxp64&feature=youtu.be"/>
 				</div>
 
 			</div>
